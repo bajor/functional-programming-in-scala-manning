449e0ce9547f354a9d181cf90cb5c005c089f921
Maciek Bajor <maciekbajor@gmail.com>
2024-07-11 23:47:40 +0200

    add

--- chapter_1/1_1.scala
+++ chapter_1/1_1.scala
@@ -2,7 +2,19 @@
 
 object ExOneOne extends App {
 
+/*
+Side effects (examples): 
+  - Modifying a variable
+  - Modifying a data structure in place
+  - Setting a field on an object
+  - Throwing an exception or halting with an error
+  - Printing to the console or reading user input
+  - Reading from or writing to a file
+  - Drawing on the screen 
+*/
 
-  println("something")
+
+// FINISHED HERE:
+// Listing 1.2 Adding a Payments object
+
 }
-
