3e4f78218695024676015bc6bb39ae2cbaca8f25
Maciek Bajor <maciekbajor@gmail.com>
2024-07-11 23:27:59 +0200

    Add

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# where I left off:
-https://livebook.manning.com/book/functional-programming-in-scala-second-edition/chapter-1/
-
-
 # book:
 https://livebook.manning.com/book/functional-programming-in-scala-second-edition/functional-programming-in-scala-second-edition/
--- chapter_1/1_1.scala
+++ chapter_1/1_1.scala
@@ -1,16 +1,7 @@
+// https://livebook.manning.com/book/functional-programming-in-scala-second-edition/chapter-1/
+
 object ExOneOne extends App {
 
-/*
-Examples of side effects:
-
-  - Modifying a variable
-  - Modifying a data structure in place
-  - Setting a field on an object
-  - Throwing an exception or halting with an error
-  - Printing to the console or reading user input
-  - Reading from or writing to a file
-  - Drawing on the screen 
-*/
 
   println("something")
 }
