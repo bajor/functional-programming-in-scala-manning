21dae757169640d1769d99ef4de96cc2fee2caf3
Maciek Bajor <maciekbajor@gmail.com>
2024-07-16 22:58:08 +0200

    adD

--- chapter_1/1_1.scala
+++ chapter_1/1_1.scala
@@ -2,17 +2,28 @@
 
 object ExOneOne extends App {
 
-/*
-Side effects (examples): 
-  - Modifying a variable
-  - Modifying a data structure in place
-  - Setting a field on an object
-  - Throwing an exception or halting with an error
-  - Printing to the console or reading user input
-  - Reading from or writing to a file
-  - Drawing on the screen 
-*/
+  /*
+    Side effects (examples):
+      - Modifying a variable
+      - Modifying a data structure in place
+      - Setting a field on an object
+      - Throwing an exception or halting with an error
+      - Printing to the console or reading user input
+      - Reading from or writing to a file
+      - Drawing on the screen
+   */
 
+  /*
+    And aren’t there other useful programs that necessitate side effects or mutation?
+
+    How do we write such programs? As we work through this book, we’ll discover how
+    many programs that seem to necessitate side effects have some functional analogue.
+
+    In other cases, we’ll find ways to structure code so that effects occur but aren’t
+    observable. (For example, we can mutate data that’s declared locally in the body of
+    some function if we ensure it can’t be referenced outside that function, or we can
+    write to a file as long as no enclosing function can observe this occurring.)
+  */
 
 // FINISHED HERE:
 // 1.2 Exactly what is a (pure) function?
