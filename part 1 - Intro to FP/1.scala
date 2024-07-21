7d745b4bf515cb3fa94244271a49c923bdd35cfe
Maciek Bajor <maciekbajor@gmail.com>
2024-07-21 12:05:28 +0200

    add

--- part 1 - Intro to FP/1.scala
+++ part 1 - Intro to FP/1.scala
@@ -11,7 +11,7 @@ object ExOneOne extends App {
       - Printing to the console or reading user input
       - Reading from or writing to a file
       - Drawing on the screen
-  */
+   */
 
   /*
     And aren’t there other useful programs that necessitate side effects or mutation?
@@ -23,44 +23,35 @@ object ExOneOne extends App {
     observable. (For example, we can mutate data that’s declared locally in the body of
     some function if we ensure it can’t be referenced outside that function, or we can
     write to a file as long as no enclosing function can observe this occurring.)
-  */
+   */
 
   /*
     A function f with input type A and output type B (written in Scala as a single type,
     A => B, which is pronounced A to B) is a computation that relates every value a of type A
     to exactly one value b of type B such that b is determined solely by the value of a.
     Any changing state of an internal or external process is irrelevant to computing the result f(a).
-    
+
     For example, a function intToSt ring having type Int => String will take every integer
     to a corresponding string. Furthermore, if it really is a function, it will do nothing else.
 
     Consider the addition (+) function on integers. It takes two integer values and returns an integer
-    value. For any two given integer values, it will always return the same integer value. 
-  */
+    value. For any two given integer values, it will always return the same integer value.
+   */
 
   /*
     We can formalize this idea of pure functions using the concept of referential
-    transparency (RT). 
-    
+    transparency (RT).
+
     This is a property of expressions in general–not just functions. For the purposes
     of our discussion, consider an expression to be any part of a program that can be evaluated
     to a result─anything you could type into the Scala interpreter and get an answer.  For example,
     2 + 3 is an expression that applies the pure function + to the values 2 and 3 (which are also
     expressions). This has no side effect. The evaluation of this expression results in the same value,
-    5, every time. 
+    5, every time.
 
 
     An expression e is referentially transparent if for all programs p, all occurrences of e in p can
     be replaced by the result of evaluating e without affecting the meaning of p. A function f is pure
     if the expression f(x) is referentially transparent for all referentially transparent x.5
-  */
-
-
-  // FINISHED HERE:
-  // 1.3 Referential transparency, purity, and the substitution model
-
-
-
-
-
+   */
 }
---
+++ part 1 - Intro to FP/2.scala
@@ -0,0 +1,5 @@
+// https://livebook.manning.com/book/functional-programming-in-scala-second-edition/chapter-2
+
+object ExOneTwo extends App {
+
+}
